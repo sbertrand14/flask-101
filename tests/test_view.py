355f4d7936b312a08f902e683fdc54c760e867c9
sbertrand14 <sbertrand14@laposte.net>
2020-01-22 14:57:01 +0100

    implemented create

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,7 +1,7 @@
 # tests/test_views.py
 from flask_testing import TestCase
-from flask import request
-from wsgi import app
+from flask import jsonify
+from wsgi import app, PRODUCTS
 
 class TestViews(TestCase):
     def create_app(self):
@@ -12,14 +12,12 @@ class TestViews(TestCase):
         response = self.client.get("/api/v1/products")
         products = response.json
         self.assertIsInstance(products, list)
-        self.assertGreater(len(products), 2) # 2 is not a mistake here.
-
+        self.assertEquals(len(products), len(PRODUCTS)) # 2 is not a mistake here.
 
     def test_one_product_json(self):
         response = self.client.get("/api/v1/products/1")
         product = response.json
         self.assert200(response)
-        print(product)
         self.assertEquals(product["id"],1)
         self.assertEquals(product["name"],"Skello")
 
@@ -27,11 +25,25 @@ class TestViews(TestCase):
         response = self.client.get("/api/v1/products/0")
         self.assert404(response)
 
-    # def test_delete_product(self):
-    #     response = self.client.delete("/api/v1/products/1")
-    #     self.assert_status(response,204)
+    def test_delete_product(self):
+        response = self.client.get("/api/v1/products/3")
+        product = response.json
+        self.assert200(response)
 
-    # def test_create_product(self):
-    #     response = self.client.post("/api/v1/products")
+        response = self.client.delete("/api/v1/products/3")
+        self.assert_status(response,204)
 
-    #     self.assert_status(response,204)
+        response = self.client.delete("/api/v1/products/3")
+        self.assert404(response)
+
+        response = self.client.get("/api/v1/products/3")
+        product = response.json
+        self.assert404(response)
+
+    def test_create_product(self):
+        newproduct = { 'id': 0, 'name': 'newproduct' }
+        response = self.client.post("/api/v1/products", json=newproduct)
+        productreturned = response.get_json()
+        self.assert_status(response,201)
+        self.assertIsInstance(productreturned, dict)
+        self.assertEquals(productreturned["id"], len(PRODUCTS))
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 # wsgi.py
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
@@ -12,6 +13,17 @@ PRODUCTS = [
     { 'id': 3, 'name': 'truc' }
 ]
 
+
+class Counter:
+    def __init__(self):
+        self.id = 3
+
+    def next(self):
+        self.id += 1
+        return self.id
+
+ID = Counter()
+
 @app.route('/api/v1/products')
 def get_products():
     return jsonify(PRODUCTS)
@@ -20,5 +32,20 @@ def get_products():
 def get_product(id):
     for x in PRODUCTS:
         if x["id"] == id:
-            return jsonify(x)
+            return make_response(jsonify(x), 200)
     abort(404)
+
+@app.route('/api/v1/products/<int:id>', methods=['DELETE'])
+def del_product(id):
+    for x in PRODUCTS:
+        if x["id"] == id:
+            PRODUCTS.remove(x)
+            return make_response("", 204)
+    abort(404)
+
+@app.route('/api/v1/products', methods=['POST'])
+def add_product():
+    newproduct = request.get_json()
+    newproduct["id"] = ID.next()
+    PRODUCTS.append(newproduct)
+    return make_response(jsonify(newproduct),201)
