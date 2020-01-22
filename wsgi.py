13875040c1f53222928e6b32be25bd7679fad7e3
sbertrand14 <sbertrand14@laposte.net>
2020-01-22 11:01:11 +0100

    GET /api/v1/products implemented

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+PRODUCTS = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' }
+]
+
+@app.route('/api/v1/products')
+def get_products():
+    return jsonify(PRODUCTS)
